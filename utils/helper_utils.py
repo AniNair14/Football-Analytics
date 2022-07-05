3ad503e7f4a08ee9327c8c36f279e15bbb795b59
AniNair14 <anishnair203@gmail.com>
2022-07-05 15:52:12 -0400

    Added convert_to_statsbomb function

---
+++ utils/helper_utils.py
@@ -0,0 +1,41 @@
+# Importing the required packages:
+import numpy as np
+import pandas as pd
+
+def convert_to_statsbomb(
+    data: pd.DataFrame,
+    x_cords: str,
+    y_cords: str
+):
+    """
+    Function to convert understat X and Y coordinate data to Statsbomb coordinates
+
+        Parameters:
+            data (pd.DataFrame): Dataframe containing X-Y data 
+            x_cords (str): X coordinate column name
+            y_cords (str): Y coordinate column name
+        
+        Returns:
+            df (pd.DataFrame): Updated Dataframe with Statsbomb X-Y coordinates
+    
+    """
+    df = data.copy()
+
+    # Convert X and Y coordinate values to numeric type:
+    df[x_cords] = pd.to_numeric(df[x_cords])
+    df[y_cords] = pd.to_numeric(df[y_cords])
+
+    df[x_cords] = df[x_cords] * 100
+    df[y_cords] = df[y_cords] * 100
+
+    df['xx'] = df[y_cords]
+    df['yy'] = df[x_cords]
+
+    df[x_cords] = df['xx']
+    df[y_cords] = df['yy']
+
+    # Rescaling the X-Y coordinates to the statsbomb scale:
+    df[x_cords] = df[x_cords] * .8
+    df[y_cords] = df[y_cords] * 1.2
+
+    return df
