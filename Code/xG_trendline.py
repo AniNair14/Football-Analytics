9a251017de5bb8b925480bb4e8d1f53622593ec2
AniNair14 <anishnair203@gmail.com>
2022-07-08 17:26:03 -0400

    Updated code to calculate rolling average

--- Code/xG_trendline.py
+++ Code/xG_trendline.py
@@ -10,3 +10,17 @@ from highlight_text import ax_text
 from highlight_text import fig_text
 
 # Loading in the Dataset:
+df = pd.read_excel('Data/Chelsea_xG.xlsx')
+
+# Calculating 10 game Rolling Average for xG For and xG Against:
+df['roll_xGF'] = df['xG'].rolling(
+    window=10,
+    min_periods=0
+).mean()
+
+df['roll_xGA'] = df['xGA'].rolling(
+    window=10,
+    min_periods=0
+).mean()
+
+# 
