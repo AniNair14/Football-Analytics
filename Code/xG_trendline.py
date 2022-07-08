cda19dff19fa7665756b8ae72287496ac5f979a5
AniNair14 <anishnair203@gmail.com>
2022-07-08 16:31:53 -0400

    Code to create xG trendline

---
+++ Code/xG_trendline.py
@@ -0,0 +1,12 @@
+# Importing the required libraries:
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+from matplotlib import gridspec
+import matplotlib.patheffects as path_effects
+from highlight_text import HighlightText
+from highlight_text import ax_text
+from highlight_text import fig_text
+
+# Loading in the Dataset:
